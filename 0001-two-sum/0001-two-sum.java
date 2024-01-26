398e02a8b9f88100e6d5de9f3630cbf6f89bf8cc
Jason Kwon (권순재) <114002458+Jason-Kwonn@users.noreply.github.com>
2024-01-26 15:05:58 +0900

    Time: 31 ms (52.4%), Space: 44.8 MB (42.69%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -12,5 +12,16 @@ class Solution {
             }
         }
         return new int[0];
+
+        // Another solution
+        // for (int i = 1; i < nums.length; i++) {
+        //     for (int j = i; j < nums.length; j++) {
+        //         if (nums[j] + nums[j - i] == target) {
+        //             return new int[] { j, j - i };
+        //         }
+        //     }
+        // }
+
+        // return null;
     }
 }
