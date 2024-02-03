0c05b34f43f49576ced771b3aa94c449eb81ed4a
Jason Kwon (권순재) <114002458+Jason-Kwonn@users.noreply.github.com>
2024-02-03 19:26:50 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] array) {
+        int maxNumber = 0;
+        int maxIndex = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > maxNumber) {
+                maxNumber = array[i];
+                maxIndex = i;
+            }
+        }
+
+        int[] result = {maxNumber, maxIndex};
+        return result;
+    }
+}
