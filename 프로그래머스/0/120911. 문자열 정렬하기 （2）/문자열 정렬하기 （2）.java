466d1ceb8318a8e3637e268ef37154c540024387
Jason Kwon (권순재) <114002458+Jason-Kwonn@users.noreply.github.com>
2024-02-12 12:10:13 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.40 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        // 소문자로 변환
+        answer = my_string.toLowerCase();
+        
+        // 문자 배열로 변환
+        char[] temp = answer.toCharArray();
+        
+        // 오름차순 정렬
+        Arrays.sort(temp);
+        
+        // String 으로 변환
+        answer = new String(temp);
+        
+        return answer;
+    }
+}
