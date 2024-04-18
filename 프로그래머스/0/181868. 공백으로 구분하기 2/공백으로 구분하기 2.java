536e3f8dd46fec49c66cc9af65c88dd9cc3a4165
Jason Kwon (권순재) <114002458+Jason-Kwonn@users.noreply.github.com>
2024-04-18 14:26:01 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.05 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public String[] solution(String my_string) {
+        List<String> list = new ArrayList<>();
+        
+        String[] newString = my_string.split(" ");
+        
+        for(String str : newString){
+            if(!str.equals("")){
+                list.add(str);
+            }
+        }
+ 
+        return list.toArray(new String[0]);
+    }
+}
