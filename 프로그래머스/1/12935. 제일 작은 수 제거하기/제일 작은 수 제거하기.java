a70173f455bb1aa73157eff5602cf57569a1f879
Jason Kwon (권순재) <114002458+Jason-Kwonn@users.noreply.github.com>
2024-05-11 07:36:18 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.04 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] solution(int[] arr) {
+        if (arr.length == 1) {
+            return new int[]{-1};
+        }
+
+        int min = Integer.MAX_VALUE;
+        for (int num : arr) {
+            if (num < min) {
+                min = num;
+            }
+        }
+
+        int[] answer = new int[arr.length - 1];
+        
+        int index = 0;
+        for (int num : arr) {
+            if (num != min) {
+                answer[index++] = num;
+            }
+        }
+
+        return answer;
+    }
+}
